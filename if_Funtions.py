05c3952f6f92f724166061332a4ec498a88a0caa
MMuthumbi <muthumbi.githinji@gmail.com>
2021-05-12 22:56:01 +0300

    IF,Elif

---
+++ if_Funtions.py
@@ -0,0 +1,28 @@
+# if elif else Funtions
+z = 4
+if z % 2 ==0 : #True
+    print("Z is even")
+    print("")
+
+y = 80
+if y % 2 ==0 : #True
+    print("Checking " + str(y))
+    print("Y is even")
+    print("")
+
+    v = 5
+if v % 2 ==0 : #False
+    print("V is even")
+    print("")
+else :
+    print("V is Odd!")
+    print("")
+    
+    q = 3
+if q % 2 == 0 :
+    print("Q is Divisible by 2.") #False
+elif q % 3 == 0 :
+    print("Q is Divisible by 3.") #True
+else :
+    print("Q is neither divisible by 2 nor by 3!")
+    print("")
